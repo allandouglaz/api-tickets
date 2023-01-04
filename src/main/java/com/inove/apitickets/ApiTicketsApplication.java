a2d131a7f71068f460a7e377a0909ca92d235cea
Allan Douglas <allanziinnhoo@gmail.com>
2023-01-04 16:47:23 -0300

    commit initial

---
+++ src/main/java/com/inove/apitickets/ApiTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.inove.apitickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTicketsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/inove/apitickets/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.inove.apitickets;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApiTicketsApplication.class);
+	}
+
+}
